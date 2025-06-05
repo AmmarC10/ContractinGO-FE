a6f08f7637d889fea732b4b4102cc0cab95d137f
Ammar Chaudhry <ammarsc02@gmail.com>
2025-06-04 21:04:59 -0400

    front page design

--- contractingo/src/app/page.tsx
+++ contractingo/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./components/layout";
+import { HeroSearch } from "./components/search/HeroSearch";
+import { JobSeekerCTA } from "./components/search/JobSeekerCTA";
 
 export default function Home() {
   return (
-   <main>
-    <Header />
-   </main>
+    <main className="min-h-screen bg-gray-50">
+      <Header />
+      <HeroSearch />
+      <JobSeekerCTA />
+    </main>
   );
 }
